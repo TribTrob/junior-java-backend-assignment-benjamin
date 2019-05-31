d85373b6a59c264dbc8bc4940345f238fea77055
TribTrob <trib073@gmail.com>
2019-05-31 17:15:30 +0200

    Added Transient annotation so the longest palindrome field is not saved in the DB

--- src/main/java/io/falcon/assignment/model/Payload.java
+++ src/main/java/io/falcon/assignment/model/Payload.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotEmpty;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -29,5 +30,6 @@ public class Payload {
     @JsonProperty("timestamp")
     @NotEmpty(message = "Please provide a timestamp")
     private String timestamp;
+    @Transient
     private int longest_palindrome_size;
 }
